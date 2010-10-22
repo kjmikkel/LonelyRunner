210ace2308de2c5b91758580fedf43b3aade9e6d
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2010-10-22 23:36:26 +0200

    Finally got the ultimemate test to compile

--- src/C++/main_test.cpp
+++ src/C++/main_test.cpp
@@ -33,19 +33,103 @@ void add_to_json_object(json_object* json_obj, unsigned long* array, int needed_
   json_object_object_add(json_obj, name.c_str(), json_array);
 }
 
-void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int speed_num, string name) {  
+void recursive_array(int* array, int* number_array, 
+		     int count_up, int last_index, 
+		     int count_down, int num_runners) {
+
+  for(int index = last_index + 1; index < count_up + 1; index++) {
+    
+    cout << "Index: " << index << ", " << count_up << "\n";
+    array[last_index + 1] = number_array[index];
+    
+    if(count_down > 0) {
+      recursive_array(array, number_array, 
+		      count_up + 1, last_index + 1, 
+		      count_down - 1, num_runners);
+    } else {
+     
+      geo_time_result* geo_result = Geometric_method(array,		     
+						     num_runners);
+    
+
+      // We check for errors
+      if(!geo_result->result || !isValid(geo_result, 
+					 number_array, 
+					 num_runners)) {
+	
+	printf("error: %d, %d\n", geo_result->result, isValid(geo_result, 
+							      number_array, 
+							      num_runners));
+	cout << "For the values: [";
+	stringstream ss;
+	
+	
+	for(int index = 0; index < num_runners; index++) {
+	  ss << array[index];
+	  
+	  cout << ", " << ss.str();
+	}
+	cout << "]\n\n";
+      }
+    }
+  }
+}
+
+// This will test every single possible combination of speeds under or equal to 100 with 10 runners 
+void ultimateTest() {
+  struct timeval start;
+  struct timeval end;
+  
+  struct timezone tz;
+  struct tm *tm;
+
+  // The array which are going to contain all the different permutations of speeds below 100
+  int array_number = 10;
+  int test_array[array_number];
+
+  // The array that is going to contain the values we are going to check
+  int max_number = 100;
+  int real_number_array[max_number];
+  
+  for(int index = 0; index < max_number; index++) {
+    real_number_array[index] = 2 + index;
+  }
+  
+  // Now to populate the array with 10 values
+  gettimeofday(&start, &tz);
+  recursive_array(test_array, real_number_array, 
+		  max_number - array_number - 1, 
+		  -1,
+		  array_number,
+		  max_number);
+  gettimeofday(&end, &tz);
+  cout << "\nDone. Making this took ";
+  
+  stringstream ss;
+  ss << (end.tv_usec - start.tv_usec + (end.tv_sec - start.tv_sec) * 1000000);
+  cout << ss.str() << " microseconds.";
+
+}
+
+void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int speed_num, int offset, string name) {  
   for(int runner_index = 0; runner_index < runner_num; runner_index++) {
     
     stringstream ss;
     ss << runners[runner_index];
     
     
-    string filename = "../data/test" + name + ss.str() + "runners.json";
+    string filename = "../data/test_" + name + "_" + ss.str() + "_runners";
+    if (offset == 0) {
+      filename += ".json";
+    } else {
+      ss << offset;
+      filename += "_with_offset_" + ss.str() + ".json";
+    }
     
     int start_speed_index = 0;
     int num_speeds = 0;
     
-    // We find out how many cells of the array have speeds that are greater or equal to the numbero 
+    // We find out how many cells of the array have speeds that are greater or equal to the number
     for(int speed_index = 0; speed_index < speed_num; speed_index++) {
       if (speeds[speed_index] >= runners[runner_index]) {
 	start_speed_index = speed_index;
@@ -102,6 +186,8 @@ void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int s
       struct timezone tz;
       struct tm *tm;
       
+      cout << "before geo\n";
+
       gettimeofday(&start, &tz);
       geo_time_result* geo_result = Geometric_method(runner_speeds, num_runners);
       gettimeofday(&end, &tz); 
@@ -121,9 +207,9 @@ void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int s
 	// to be able to differentiate it from all the other values we set it to 0
 	geo_error[real_index] = 0;
       }
-      
+      /* 
       gettimeofday(&start, &tz);
-      num_time_result* num_result = Numerical_method(runner_speeds, num_runners);
+           num_time_result* num_result = Numerical_method(runner_speeds, num_runners, true, false);
       gettimeofday(&end, &tz);
       num_results[real_index] = (unsigned long)(end.tv_usec - start.tv_usec + (end.tv_sec - start.tv_sec) * 1000000);
       
@@ -136,7 +222,7 @@ void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int s
       }
       
       
-      cout << "geo: " <<  geo_results[real_index] << ", num: " << num_results[real_index] << "\n";
+      cout << "geo: " <<  geo_results[real_index] << ", num: " << num_results[real_index] << "\n";*/
     }
     
     // Create the json object we are going to store the tests in
@@ -161,34 +247,39 @@ void doTest(int* runners, int* speeds, int* actual_speeds, int runner_num, int s
   }
 }
 
-int main (int argc, char *argv[]) {
+
+void sequential_prime_test() {
+  const int runner_num = 8;
+  const int offset = 5;
+  const int speed_num = 80;
+  int max_number = 500000;
   
-  const int runner_num = 1;
-  const int speed_num = 50;
-  int max_number = 5000000;
-  
-  //  int runners[runner_num] = {10, 50, 100, 500, 1000, 2000};
-  //  runner_num = 2;
-  int runners[runner_num] = {5000};
+  // The number of runners
+  int runners[runner_num] = {10, 50, 100, 500, 1000, 2000, 4000, 8000};
   int speeds[speed_num]; 
+  
+  // The array contains the speeds we are going to test
   for(int speed_index = 0; speed_index < speed_num; speed_index++) {
-    speeds[speed_index] = (speed_index + 1) * 100;
+    speeds[speed_index] = (speed_index + 1) * 100 + offset;
   }
 
   // We find the primes which are going as the speeds
   int* primes = findPrimes(max_number);
-  //  doTest(runners, speeds, primes, runner_num, speed_num, "Primes");
+  //  doTest(runners, speeds, primes, runner_num, speed_num, offset, "Primes");
   printf("done prime\n");
   
   free(primes);
   
-  max_number = 5000;
   int sequential_numbers[max_number];
-  for(int seq_index = 1; seq_index <= max_number; seq_index++) {
+  for(int seq_index = 1; seq_index <= max_number - offset; seq_index++) {
     sequential_numbers[seq_index - 1] = seq_index;
   }
-  sequential_numbers[max_number - 1] = 1000000;
   
-  doTest(runners, speeds, sequential_numbers, runner_num, speed_num, "Sequential");
-    
+  doTest(runners, speeds, sequential_numbers, runner_num, speed_num, offset, "Sequential");
+  free(sequential_numbers);
+}
+
+int main (int argc, char *argv[]) {
+  //  sequential_prime_test();
+  ultimateTest();
 }
