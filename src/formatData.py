284a10844154c1465fc0216ca54d7e38a1f80fd4
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2010-11-05 15:33:43 +0100

    Better formatting - both for tables and graphs

--- src/formatData.py
+++ src/formatData.py
@@ -12,7 +12,7 @@ class bar_instance:
 	def __str__(self):
 		return "Speed: " + str(self.speed) + ", Geometrical time: (" + str(self.geo) + ", " + str(self.geo_spread) + ", Numerical time:(" + str(self.num) + ", " + str(self.num_spread) + ")"
 
-inData = "data Backup/"
+inData = "data/"
 outData = "../report/data/"
 graphs = outData + "graphs/"
 tables = outData + "tables/"
@@ -26,8 +26,8 @@ def saveFile(filename, data):
 	file.write(data)
 	file.close()	
 
-def doGnuPlot(filename, extra_name, maxYValue):
-	
+def doGnuPlot(filename, extra_name, maxYValue, max_speed_to_test):
+
 	DATAFILE='xrddata.dat'
 	PLOTFILE='xrddata.ps'
 
@@ -38,20 +38,25 @@ def doGnuPlot(filename, extra_name, maxYValue):
 	
 	m = re.search('[a-zA-Z_]+([0-9]+)[a-zA-Z_]+', title)
 	title = title.replace("_", " ")
-	
+	title = title.replace("test", "")
+	title = title.strip()
 	minNum = m.group(1)
 
+	if int(minNum) >= max_speed_to_test:
+		return
+	
+	
 	input_filename = filename + ".dat"
-
+	
 	f=os.popen('gnuplot' ,'w')
-	print >>f, "set xrange [%f:%f]" % (int(minNum), 5000)
+	print >>f, "set xrange [%f:%f]" % (int(minNum), max_speed_to_test)
 	print >>f, "set yrange [%f:%f]" % (0, maxYValue)
-	print >>f, "set title %s" % title
+	print >>f, "set title '%s'" % title
 	print >>f, "set term postscript"
 	print >>f, "set out '%s'" % (filename + "_" + extra_name + ".ps")
+
 	print >>f, "set noborder"
-	print >>f, "set multiplot"
-	
+	print >>f, "set multiplot"	
 	print >>f, "set xlabel 'Maximum speed'; set ylabel 'Number of microseconds'"	
 	print >>f, "plot '%s' using 1:2:3 with errorbars title 'Geometrical algorithm' lc rgb 'red', '%s' using 1:4:5 with errorbars title 'Numerical algorithm' lc rgb 'green'" % (input_filename, input_filename)
 	print >>f, "unset multiplot"
@@ -62,20 +67,20 @@ def print_tables(info_list, filename):
 	filename = tables + os.path.basename(filename).split('.')[0] + ".tex"
 
 	newline = "\\\\\n"
-	header = "Speed & Geometrical & Numerical"
-	strAccum = 	"\\begin{tabular}[6]{c|c|c||c|c|c}\n" + header + " & " + header + newline + "\hline\n"
+	header = "Speed & Geometrical & Geo Spread & Numerical & Num Spread"
+	strAccum = 	"\\begin{tabular}[6]{c|c|c|c|c||c|c|c|c|c}\n" + header + " & " + header + newline + "\hline\n"
 	
 	halflength = len(info_list) / 2
 	
 	for num in range(0, halflength):
 		half_num = num + halflength
 		info = info_list[num]
-		first_half = str(info.speed) + " & " + str(info.geo) + " & " + str(info.num)		
+		first_half = str(info.speed) + " & " + str(info.geo) + " & " + str(info.geo_spread) + " & " + str(info.num) + " & " + str(info.num_spread)		
 		if len(info_list) < half_num:		
 			strAccum += first_half + " &  & & " + newline
 		else:
 			info2 = info_list[half_num]
-			second_half = str(info2.speed) + " & " + str(info2.geo) + " & " + str(info2.num)			
+			second_half = str(info2.speed) + " & " + str(info2.geo) + " & " + str(info2.geo_spread) + " & " + str(info2.num) + " & " + str(info2.num_spread)			
 			strAccum += first_half + " & " +  second_half + newline
 	strAccum += "\end{tabular}"
 	saveFile(filename, strAccum)
@@ -86,14 +91,17 @@ def print_plot(info_list, filename):
 	strAccum = "#Speed\tGeo\tGeo_spread\tNum\tNum_spread\n"
 	maxGeoValue = -1	
 	maxNumValue = -1
+	max_speed_to_test = 5000	
 	for bar in info_list:
+		if bar.speed > max_speed_to_test:
+			break;
 		strAccum += str(bar.speed) + "\t" + str(bar.geo) + "\t" + str(bar.geo_spread) + "\t" + str(bar.num) + "\t" + str(bar.num_spread) +"\n"
-		maxGeoValue = max(maxGeoValue, bar.geo)
-		maxNumValue = max(maxNumValue, bar.num)
+		maxGeoValue = max(maxGeoValue, bar.geo + bar.geo_spread)
+		maxNumValue = max(maxNumValue, bar.num + bar.num_spread)
 		
 	saveFile(filename + ".dat", strAccum)
-	doGnuPlot(filename, "Geo", maxGeoValue)
-	doGnuPlot(filename, "Num", maxNumValue)
+	doGnuPlot(filename, "Geo", maxGeoValue, max_speed_to_test)
+	doGnuPlot(filename, "Num", maxNumValue, max_speed_to_test)
 	
 
 def processData():
@@ -113,7 +121,7 @@ def processData():
 		for index in range(0, len(numerical)):
 			bar_list.append(bar_instance(speeds[index], geometrical[index], geometrical_spread[index], numerical[index], numerical_spread[index]))
 		
-		print_plot(bar_list, infile)
+#		print_plot(bar_list, infile)
 		print_tables(bar_list, infile)		
 		
 processData()
